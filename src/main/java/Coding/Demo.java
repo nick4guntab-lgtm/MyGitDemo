d6f6d55a2d72f349aedc5d812efb025be730da96
Nikhil Gudelli <nick4guntab@gmail.com>
2026-04-27 17:06:00 +0530

    added couple of lines for syso

--- src/main/java/Coding/Demo.java
+++ src/main/java/Coding/Demo.java
@@ -4,7 +4,9 @@ public class Demo {
 	public static void main(String[] args) {
 		
 		System.out.println("Hello World");
-		
+		System.out.println("Hello World");
+		System.out.println("Hello World");
+		System.out.println("Hello World");
 	}
 
 }
