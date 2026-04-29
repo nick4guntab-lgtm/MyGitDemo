cb3c0bbd7348ca95447cbe9d298bd87a9f747808
Nikhil Gudelli <nick4guntab@gmail.com>
2026-04-29 14:12:57 +0530

    made changes in gitX

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -7,19 +7,23 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 /**
  * Unit test for simple App.
  */
-public class AppTest
-{
-    @Test
-    public void testAppConstructor() {
-        App app1 = new App();
-        App app2 = new App();
-        assertEquals(app1.getMessage(), app2.getMessage());
-    }
+public class AppTest {
+	@Test
+	public void testAppConstructor() {
+		App app1 = new App();
+		App app2 = new App();
+		assertEquals(app1.getMessage(), app2.getMessage());
+	}
 
-    @Test
-    public void testAppMessage()
-    {
-        App app = new App();
-        assertEquals("Hello World!", app.getMessage());
-    }
+	@Test
+	public void testAppMessage() {
+		App app = new App();
+		assertEquals("Hello World!", app.getMessage());
+	}
+
+	public static void main(String[] args) {
+
+		System.out.println("Change from GitX");
+
+	}
 }
